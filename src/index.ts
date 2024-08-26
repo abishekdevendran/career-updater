9b32f9d9f888280ed89fb0c997380706cec460ac
Abishek Devendran <abishekdevendran@gmail.com>
2024-08-26 21:39:05 +0530

    Fixed diffing

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,30 @@ interface Env {
 	// ... other binding types
 }
 
+// a function that takes in markup as string, and strips away all content between <script> tags and <style> tags
+function stripScriptAndStyle(markup: string): string {
+	return (
+		markup
+			.replace(/<script.*?<\/script>/gs, '')
+			.replace(/<style.*?<\/style>/gs, '')
+			.replace(/<footer.*?<\/footer>/gs, '')
+			.replace(/<nav.*?<\/nav>/gs, '')
+			// comments
+			.replace(/<!--.*?-->/gs, '')
+			// symbols
+			.replace(/<symbol.*?<\/symbol>/gs, '')
+			// fill
+			.replace(/<fill.*?<\/fill>/gs, '')
+			// noscript
+			.replace(/<noscript.*?<\/noscript>/gs, '')
+			// svg
+			.replace(/<svg.*?<\/svg>/gs, '')
+			// \t and \n
+			.replace(/\t/g, '')
+			.replace(/\n/g, '')
+	);
+}
+
 export default {
 	// The scheduled handler is invoked at the interval set in our wrangler.toml's
 	// [[triggers]] configuration.
@@ -35,11 +59,11 @@ export default {
 		const promises = Data.map(async (source) => {
 			const response = await fetch(source.url);
 			// We'll use the NodeHtmlMarkdown library to convert the HTML to Markdown
-			const markdown = await response.text();
-			console.log('RAW: ', markdown);
+			const markdown = stripScriptAndStyle(await response.text());
 			// check if the source.name exists in KV
 			const KVval = await env.CAREER_KV.get(source.name);
 			if (KVval === null) {
+				console.log('KVval is null for ', source.name, ' putting in KV');
 				// If it doesn't exist, we'll store the response in KV
 				await env.CAREER_KV.put(source.name, markdown);
 				// return diff, which is empty here
@@ -48,6 +72,7 @@ export default {
 			// If it does exist, we'll compare the new response to the old response
 			// calculate the diff
 			const diff = Diff.diffChars(KVval, markdown);
+			console.log('Diff: ', diff);
 			// If the diff is empty, we'll return null
 			if (diff.length === 1) {
 				return null;
@@ -79,13 +104,17 @@ export default {
 				if (result.join('').replace(/[^0-9]/g, '').length > result.join('').replace(/[^a-zA-Z]/g, '').length) {
 					return null;
 				}
+				// if the string doesn't have html tags, we'll return null
+				if (!result.join('').includes('<')) {
+					return null;
+				}
 				return `<h2>${Data[index].name}</h2><pre>${result.join(' ')}</pre>`;
 			})
 			.filter((result) => result !== null);
 
 		console.log('Processed: ', processed);
 
-		// // If there are no results, we'll return early
+		// If there are no results, we'll return early
 		// if (processed.length === 0) {
 		// 	return;
 		// }
@@ -94,13 +123,17 @@ export default {
 		const discordWebhook = env.DISCORD_WEBHOOK;
 		// console.log('Discord Webhook: ', discordWebhook);
 		const discordBody = {
-			content: 'New job postings detected!',
+			content: processed && processed.length > 0 ? 'New job postings detected!' : 'No new job postings detected!',
 			embeds:
 				processed && processed.length > 0
 					? processed.map((result) => ({
 							description: result,
 					  }))
-					: '<h2>No new job postings detected</h2>',
+					: [
+							{
+								description: 'No new job postings detected!',
+							},
+					  ],
 		};
 
 		const finalresp = await fetch(discordWebhook, {
@@ -112,7 +145,17 @@ export default {
 		});
 
 		// check if the response was successful
-		const wasSuccessful = finalresp.status
+		const wasSuccessful = finalresp.ok;
+		if (!wasSuccessful) {
+			try {
+				const err = await finalresp.json();
+				console.log('Error: ', err);
+			} catch (e) {
+				console.log('Error: ', e);
+				const err = await finalresp.text();
+				console.log('Error: ', err);
+			}
+		}
 		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
 
 		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
